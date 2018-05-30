5b51d32895e2bf4a395f696931652c75a5911b06
kjb244 <kevin.bacino@gmail.com>
2018-05-30 18:55:47 -0400

    initial commit

---
+++ src/components/CurrencyInput.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="parent-currency-wrapper">
+    <input type="text" v-on:input="valueChange($event)" :placeholder="currencyprops.placeholder" v-on:blur="valueBlur($event)" >
+  </div>
+</template>
+
+
+<script>
+  export default {
+    name: 'currencyinput',
+    props: ['currencyprops'],
+    data(){
+      return{
+        inputModel: this.currencyprops.value || '',
+      }
+    },
+    methods: {
+
+      valueBlur: function(e){
+        const target = e.target;
+        if(target.value.includes('.')){
+          let parts = target.value.split('.');
+          parts[1] = this.padZeros(parts[1]);
+          target.value = `${parts[0]}.${parts[1]}`;
+        }
+      },
+      valueChange: function(e){
+        const target = e.target;
+        target.value = this.currencyRules(target.value);
+      },
+      currencyRules: function(value){
+        let valu = value || '';
+        let goodValue = valu.replace(/[^0-9\.]/g,'');
+        //strip more than one period
+        const periods = goodValue.length - goodValue.replace(/\./g,'').length;
+        if(periods > 1){
+          for(let i=0; i<periods-1;i++){
+            goodValue = goodValue.replace(/\./,'');
+          }
+        }
+
+        const parts = goodValue.split('.');
+        let firstPart = '';
+        let secondPart = '';
+        //add commas
+        firstPart = parts[0].split('').reverse().map((e,i) => {
+            if(i>0 && i%3 === 0){
+              return  e + ',';
+            }
+            return e;
+          }).reverse().join('');
+        secondPart = parts.length > 1 ? `.${parts[1]}` : '';
+
+        return `${firstPart}${secondPart}`;
+
+      },
+      padZeros: function(inp){
+        inp = inp || '';
+        if(inp.length === 0){
+          return '00';
+        }
+        else if (inp.length === 1){
+          return inp + '0';
+        }
+
+        return inp.substr(0,2);
+      }
+
+
+    },
+    created: function(){
+
+    },
+    computed:{
+
+    },
+    mounted: function(){
+
+    }
+  }
+</script>
+
+<style scoped>
+  input{
+    text-indent: 10px;
+  }
+
+
+</style>
