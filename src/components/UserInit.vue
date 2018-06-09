fe3e7d930d8e9e36d4dd3563826e4c59a6d0e84e
kjb244 <kevin.bacino@gmail.com>
2018-06-09 08:09:57 -0400

    firebase additional updates

--- src/components/UserInit.vue
+++ src/components/UserInit.vue
@@ -20,10 +20,10 @@
               <div class="card-body">
                 <div class="row mb-3" v-for="(cost, index2) in rec.costs">
                   <div class="col-sm-12">
-                    <input type="text" placeholder="for what" :disabled="cost.disabled" v-model="cost.what"/>
+                    <input type="text" placeholder="for what" :disabled="cost.disabled" v-model="cost.what" v-on:change="dbOnChange('what',index,index2,cost)"/>
                   </div>
                   <div class="col-sm-12 mt-2">
-                    <currencyinput :currencyprops="currencyProps" :disabled="cost.disabled" v-model="cost.amount"></currencyinput>
+                    <currencyinput :currencyprops="{placeholder: currencyProps.placeholder, value: cost.amount}" :disabled="cost.disabled"  v-model="cost.amount" v-on:input="test"></currencyinput>
 
                   </div>
                   <div class="col-sm-12 mt-2">
@@ -62,7 +62,8 @@
 <script>
   import currencyinput from './CurrencyInput.vue';
   import utils from '../utils/Utils';
-  import firebase from 'firebase'
+  import firebase from 'firebase';
+
 
   export default {
     name: 'userinit',
@@ -79,24 +80,52 @@
         payeeModel: [[[]]],
         currencyProps: {
           placeholder: 'Enter amount',
-          value: ''
         }
       }
     },
+    watch: {
+
+
+
+
+    },
     methods: {
-      dbAddNames: function(commaNamesArr){
+      test: function(val){
+        console.log(val);
+      },
+      dbOnChange: function(type, index, index2, cost){
         const db = firebase.database();
         const urlKey = this.$route.query.urlKey;
-        db.ref().child('/master/urlKeyMap/' + urlKey).set({users: commaNamesArr.map(e => e.trim())});
-        const map = commaNamesArr.reduce((accum,val) => {
-          accum[val.trim()] = {costs: false};
+        const name = this.arrNames[index].name;
+        const obj = type === 'what' ? {what: cost.what} : {amount: cost.amount};
+        db.ref().child(`/master/costMap/${urlKey}/${name}/costs/${index2}`).update(obj);
+      },
+      dbRemoveAllListeners: function(){
+        const db = firebase.database();
+        const urlKey = this.$route.query.urlKey;
+        this.arrNames.map((e)=> {
+          db.ref(`master/costMap/${urlKey}/${e.name}/costs`).off();
+        });
+
+      },
+      dbWriteAll: function(){
+        const db = firebase.database();
+        const urlKey = this.$route.query.urlKey;
+        db.ref().child('/master/urlKeyMap/' + urlKey).set({users: this.arrNames.map( e => e.name)});
+        const map = this.arrNames.reduce((accum,val, idx) => {
+          accum[val.name] = {costs: val.costs.map( (e, idx2) => {
+            delete e.disabled;
+            e.payeeArr = this.payeeModel[idx][idx2];
+            return e;
+          })};
           return accum;
+
         },{});
+
         db.ref().child('/master/costMap/' + urlKey).set(map);
       },
       namesToArray: function(){
         const commaNamesArr = this.commaNames.split(',');
-        this.dbAddNames(commaNamesArr);
         this.arrNames = commaNamesArr.map((e,i) =>{
           this.payeeModel.push([]);
           return {
@@ -164,50 +193,101 @@
         }
       },
       calculateCosts: function(){
-        utils.vueStore.setStore('calculateCosts', {'arrNames': this.arrNames, 'payeeModel':  this.payeeModel});
-        window.location.hash = '#/calculate';
+        //utils.vueStore.setStore('calculateCosts', {'arrNames': this.arrNames, 'payeeModel':  this.payeeModel});
+        //window.location.hash = '#/calculate';
+        this.dbRemoveAllListeners();
+        this.dbWriteAll();
       }
+
 
     },
     created: function(){
       let db = firebase.database();
       const urlKey = this.$route.query.urlKey;
-      const initalObj = {urlKeys: {[urlKey]:''},
-                        urlKeyMap: {[urlKey]:
-                                      { users: false}},
-                        costMap: {[urlKey]: false}};
+      const self = this;
+      const createdDt = new Date();
 
-      db.ref('master').once('value', snapshot => {
-        //if we don't have master node then create
-        if(!snapshot.val()){
-          db.ref('master/').update(initalObj);
-        }
-        //if we don't have key then add it
-        else{
-          db.ref('master').child('urlKeys').once('value', snapshot2 => {
-            const urlKeys = (snapshot2.val() || {});
-            if(urlKeys && !(urlKey in urlKeys)){
-              db.ref().child('/master/urlKeys/' + urlKey).set(false);
-              db.ref().child('/master/urlKeyMap/' + urlKey).set({users: false});
-              db.ref().child('/master/costMap/' + urlKey).set(false);
-            }
-            //if we're here then we have data for this urlKey so populate model
-            else {
-              const node = snapshot.val();
-              this.commaNames = node.urlKeyMap[urlKey].users.join(',');
+      init();
 
 
+
+      function addListeners(){
+        const arr = self.arrNames;
+
+        arr.map((e) => {
+          db.ref(`master/costMap/${urlKey}/${e.name}/costs`).off();
+          db.ref(`master/costMap/${urlKey}/${e.name}/costs`).on('child_added', (snapshot) => {
+            const listenerDate = new Date();
+            if(listenerDate.getTime()/1000 - createdDt.getTime()/1000 <2){
+              return false;
             }
 
-          })
-        }
-      })
+            const val = snapshot.val();
+            arr.find(data => data.name === e.name).costs.push(val);
+          });
+          db.ref(`master/costMap/${urlKey}/${e.name}/costs`).on('child_removed', (snapshot) => {
+            console.log('changed removed', e.name, snapshot.val());
+          });
+        });
+
+      }
+
+      function init() {
+        const initialObj = {
+          urlKeys: {[urlKey]: ''},
+          urlKeyMap: {
+            [urlKey]:
+              {users: false}
+          },
+          costMap: {[urlKey]: false}
+        };
+
+        db.ref('master').once('value', snapshot => {
+          //if we don't have master node then create
+          if (!snapshot.val()) {
+            db.ref('master/').update(initialObj);
+          }
+          //if we don't have key then add it
+          else {
+            db.ref('master').child('urlKeys').once('value', snapshot2 => {
+              const urlKeys = (snapshot2.val() || {});
+              if (urlKeys && !(urlKey in urlKeys)) {
+                db.ref().child('/master/urlKeys/' + urlKey).set(false);
+                db.ref().child('/master/urlKeyMap/' + urlKey).set({users: false});
+                db.ref().child('/master/costMap/' + urlKey).set(false);
+              }
+              //if we're here then we have data for this urlKey so populate model
+              else {
+                const node = snapshot.val();
+                self.commaNames = node.urlKeyMap[urlKey].users.join(',');
+                const costMapUsers = node.costMap[urlKey];
+                Object.keys(costMapUsers).map((e, i) => {
+                  self.payeeModel.push([]);
+                  const entry = costMapUsers[e];
+                  const costArr = entry.costs.map((e, i2) => {
+                    self.payeeModel[i][i2] = e.payeeArr;
+                    return Object.assign({}, e, {disabled: false});
+                  });
+                  const obj = {name: e, id: e.trim().replace(/\s+/g, '') + i, costs: costArr};
+                  self.arrNames.push(obj);
+                });
+                addListeners();
+              }
+            })
+          }
+        });
+      }
+
+
+
+
 
 
     },
     computed:{
 
     },
+
     mounted: function(){
 
     }
